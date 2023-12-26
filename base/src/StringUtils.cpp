dad28cbf580931952dd4ee847fce07d7be5df9d6
shun-sfoo <ganymede0915@gmail.com>
2023-12-26 22:01:55 +0800

    books:tdd

--- base/src/StringUtils.cpp
+++ base/src/StringUtils.cpp
@@ -7,7 +7,7 @@ namespace base {
 
 std::string executeCmd(const std::string &cmd) {
   const int bufferSize = 128;
-  std::array<char, bufferSize> buffer;
+  std::array<char, bufferSize> buffer{};
   std::string result;
   std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
   if (!pipe) {
---
+++ executable/test1.cpp
@@ -0,0 +1,16 @@
+struct Widget {
+  int m_M;
+};
+
+class Demo {
+
+private:
+  int m_Data;
+};
+
+int test(int Num) {
+  int a = 3;
+  return Num * a * 5;
+}
+
+int main() { return 0; }
